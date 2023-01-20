c459790c9df2b55d2602a735836ee2c1d6200dce
Danakane <dal.li@outlook.com>
2023-01-20 14:33:00 +0100

    improve prompt for linux

--- platform/linux.py
+++ platform/linux.py
@@ -16,10 +16,11 @@ from platform import Platform, LINUX
 class Linux(Platform):
 
     PROMPTS = {
-        "sh": """'$(command printf "([remote] $(whoami)@$(hostname):$PWD\\$ ")'""",
+        "sh": """'$(command printf "[remote] $(whoami)@$(hostname):$PWD\\$ ")'""",
         "dash": """'$(command printf "[remote] $(whoami)@$(hostname):$PWD\\$ ")'""",
-        "zsh": """'%B%F{red}[remote] %B%F{yellow}%n@%M%B%F{reset}:%B%F{cyan}$PWD%B%(#.%b%F{white}#.%b%F{white}$)%b%F{reset} '""",
-        "default": """'$(command printf "\\[\\033[01;31m\\][remote]\\[\\033[0m\\] \\[\\033[01;33m\\]$(whoami)@$(hostname)\\[\\033[0m\\]:\\[\\033[1;36m\\]$PWD\\[\\033[0m\\]\\$ ")'""",
+        "zsh": """'%B%F{red}[remote] %B%F{yellow}%n@%M%B%F{reset}:%B%F{cyan}%~%B%(#.%b%F{white}#.%b%F{white}$)%b%F{reset} '""",
+        "bash": """'$(command printf "\\[\\033[01;31m\\][remote] \\[\\033[0m\\]\\[\\033[01;33m\\]$(whoami)@$(hostname)\\[\\033[0m\\]:\\[\\033[1;36m\\]\w\\[\\033[0m\\]\\$ ")'""",
+        "default": """'$(command printf "\\[\\033[01;31m\\][remote] \\[\\033[0m\\]\\[\\033[01;33m\\]$(whoami)@$(hostname)\\[\\033[0m\\]:\\[\\033[1;36m\\]$PWD\\[\\033[0m\\]\\$ ")'"""
     }
 
     def __init__(self, chan: channel.Channel) -> None:

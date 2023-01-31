c1d4aeef5b5311542e9ac8e819325074f6989b87
Danakane <dal.li@outlook.com>
2023-01-31 22:43:20 +0100

    improve: command-line system refactoring

--- redcat/command.py
+++ redcat/command.py
@@ -11,27 +11,6 @@ def argument(*name_or_flags, **kwargs):
     return (list(name_or_flags), kwargs)
 
 
-def subcommand(parent, args=[]):
-    """Decorator to define a new subcommand in a sanity-preserving way.
-    The function will be stored in the ``func`` variable when the parser
-    parses arguments so that it can be called directly like so::
-        args = cli.parse_args()
-        args.func(args)
-    Usage example::
-        @subcommand(parser, [argument("-d", help="Enable debug mode", action="store_true")])
-        def subcommand(args):
-            print(args)
-    Then on the command line::
-        python cli.py subcommand -d
-    """
-    def decorator(func):
-        parser = parent.add_parser(func.__name__, description=func.__doc__)
-        for arg in args:
-            parser.add_argument(*arg[0], **arg[1])
-        parser.set_defaults(func=func)
-    return decorator
-
-
 class Command:
 
     SUBCOMMAND: str = "subcommand"
